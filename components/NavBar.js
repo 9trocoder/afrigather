c6a8ac0c1c115381f7f4fcfc8d0052cee68edd36
Emmanuel Oladimeji Adeyemi <46575938+9trocoder@users.noreply.github.com>
2022-08-24 15:50:56 +0300

    update

--- components/NavBar.js
+++ components/NavBar.js
@@ -4,13 +4,12 @@ import Link from "next/link";
 const NavBar = () => {
   return (
     <div className={styles.navvcnt}>
-        <Link href="/">
-      <h1 className={styles.logoheaderbig}>
-        AFRI. <span>Gather</span>
-      </h1>
-    </Link>
+      <Link href="/">
+        <h1 className={styles.logoheaderbig}>
+          AFRI. <span>Gather</span>
+        </h1>
+      </Link>
     </div>
-    
   );
 };
 
--- pages/blog-details/[slug].js
+++ pages/blog-details/[slug].js
@@ -1,7 +1,9 @@
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 import { createClient } from "contentful";
 import Image from "next/image";
+import Link from "next/dist/client/link";
 import Skeleton from "../../components/Skeleton";
+import styles from "../../styles/detailslug.module.css";
 
 const client = createClient({
   space: process.env.CONTENTFUL_SPACE_ID,
@@ -45,27 +47,84 @@ export async function getStaticProps({ params }) {
   return {
     props: {
       blog: items[0],
-      bloglists: res.items,
+      blogs: res.items,
     },
     revalidate: 1,
   };
 }
 
-export default function BlogDetails({ blog, bloglists }) {
+export default function BlogDetails({ blog, blogs }) {
   if (!blog) return <Skeleton />;
   const { featuredImage, title, authors, method, date } = blog.fields;
 
   return (
-    <div>
-      <h1>{title}</h1>
-      <Image
-        src={"https:" + featuredImage.fields.file.url}
-        width={featuredImage.fields.file.details.image.width}
-        height={500}
-        alt="featured image"
-      />
+    <div className={styles.blogdetailsbody}>
+      <div className={styles.blogdetailsleft}>
+        <h1>{title}</h1>
+        <Image
+          src={"https:" + featuredImage.fields.file.url}
+          width={featuredImage.fields.file.details.image.width}
+          height={500}
+          alt="featured image"
+        />
+        <div className={styles.splititblog}>
+          <div className={styles.datetimeread}>
+            <p>
+              {new Date(date).toLocaleString("en-us", {
+                month: "long",
+                day: "numeric",
+                year: "numeric",
+              })}
+            </p>
+            <p>5 min read</p>
+          </div>
+          <div className={styles.dauthor}>
+            <p>{authors[0]}</p>
+          </div>
+        </div>
 
-      <div>{documentToReactComponents(method)}</div>
+        <div>{documentToReactComponents(method)}</div>
+      </div>
+      <div className={styles.blogdetailsright}>
+        <h1 className={styles.denalsolike}>People Also read this</h1>
+        <div className={styles.splititna}>
+          {blogs.slice(6, 8).map((blog, key) => (
+            <div key={key}>
+              <Image
+                src={"https:" + blog.fields.thumbnail.fields.file.url}
+                width={blog.fields.thumbnail.fields.file.details.image.width}
+                height={400}
+                alt={blog.fields.title}
+              />
+              <div className={styles.splititblog}>
+                <div className={styles.datetimeread}>
+                  <p>
+                    {new Date(blog.fields.date).toLocaleString("en-us", {
+                      month: "long",
+                      day: "numeric",
+                      year: "numeric",
+                    })}
+                  </p>
+                </div>
+                <div className={styles.dauthor}>
+                  <p>{blog.fields.authors[0]}</p>
+                </div>
+              </div>
+              <h1 className={styles.hometitle}>{blog.fields.title}</h1>
+              <p className={styles.homeparagraph}>{blog.fields.subTItle}</p>
+              <div className={styles.readmorebtn}>
+                <Link href={"/blog-details/" + blog.fields.slug}>
+                  <a>Read now</a>
+                </Link>
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <Link href="/">
+          <p className={styles.blogslugereadl}>View All</p>
+        </Link>
+      </div>
     </div>
   );
 }
