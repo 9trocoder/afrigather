bed975527afd5c34c3174b549fc2b85c0647967b
Emmanuel Oladimeji Adeyemi <46575938+9trocoder@users.noreply.github.com>
2022-08-30 17:30:36 +0300

    added seo

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,58 @@
+import { useEffect } from "react";
+import Script from "next/script";
+import { useRouter } from "next/router";
 import Layout from "../components/Layout";
 import "../styles/globals.css";
+import * as gtag from "../lib/gtag";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageView(url);
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+    router.events.on("hashChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+      router.events.off("hashChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
   return (
-    <div className="thecontainer">
-       <Layout>
-      <Component {...pageProps} />
-    </Layout>
-    </div>
-   
+    <>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9197029786441774"
+        crossorigin="anonymous"
+      />
+
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+
+      <div className="thecontainer">
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </div>
+    </>
   );
 }
 
